de2afd0ba94853edd2ee9d25a34993401c8c2a7b
Nick Patterson <nkpatterson@Nicks-iMac.lan>
2019-03-15 15:47:45 -0700

    Tweaked the TF template to make better use of variables

--- Deployments/terraform/main.tf
+++ Deployments/terraform/main.tf
@@ -1,12 +1,17 @@
 provider "azurerm" {}
 
+locals {
+	name_prefix = "cloudskills-${var.instance_name}"
+	b2c_root_uri = "https://${var.b2c_tenant}.b2clogin.com/${var.b2c_tenant}.onmicrosoft.com"
+}
+
 resource "azurerm_resource_group" "rg" {
-	name		= "cloudskills-${var.instance_name}-rg"
+	name		= "${local.name_prefix}-rg"
 	location	= "${var.location}"
 }
 
 resource "azurerm_app_service_plan" "asp" {
-	name		= "cloudskills-${var.instance_name}-asp"
+	name		= "${local.name_prefix}-asp"
 	location	= "${azurerm_resource_group.rg.location}"
 	resource_group_name = "${azurerm_resource_group.rg.name}"
 
@@ -17,10 +22,11 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "web" {
-	name		= "cloudskills-${var.instance_name}"
+	name		= "${local.name_prefix}"
 	location	= "${azurerm_resource_group.rg.location}"
 	resource_group_name = "${azurerm_resource_group.rg.name}"
 	app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+	https_only = "true"
 
 	app_settings = {
 		"AzureCosmosDbOptions:ConnectionString" = "AccountEndpoint=${azurerm_cosmosdb_account.cosmos.endpoint};AccountKey=${azurerm_cosmosdb_account.cosmos.primary_master_key};"
@@ -29,19 +35,62 @@ resource "azurerm_app_service" "web" {
 		"AzureQueueStorageEventPublisherOptions:QueuePrefix" = ""
 		"AzureTableStorageOptions:ConnectionString" = "${azurerm_storage_account.storage.primary_connection_string}"
 		"ApplicationInsights:InstrumentationKey" = "${azurerm_application_insights.ai.instrumentation_key}"
-		"AzureADB2COptions:Authority" = "https://cloudskillsuhaul.b2clogin.com/cloudskillsuhaul.onmicrosoft.com/B2C_1_SignUpSignIn/v2.0"
-		"AzureADB2COptions:ClientId" = "fd83d2b6-f2e9-4a30-a661-ae3e59f90af5"
-		"AzureADB2COptions:ClientSecret" = ""
-		"AzureADB2COptions:MetadataAddress" = "https://cloudskillsuhaul.b2clogin.com/cloudskillsuhaul.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignUpSignIn"
+		"AzureADB2COptions:Authority" = "${local.b2c_root_uri}/${var.b2c_policy}/v2.0"
+		"AzureADB2COptions:ClientId" = "${var.b2c_client_id}"
+		"AzureADB2COptions:ClientSecret" = "${var.b2c_client_secret}"
+		"AzureADB2COptions:MetadataAddress" = "${local.b2c_root_uri}/v2.0/.well-known/openid-configuration?p=${var.b2c_policy}"
+		"APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.ai.instrumentation_key}"
+		"APPINSIGHTS_PROFILERFEATURE_VERSION" = "1.0.0"
+		"APPINSIGHTS_SNAPSHOTFEATURE_VERSION" = "1.0.0"
+		"ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
+		"DiagnosticServices_EXTENSION_VERSION" = "~3"
+		"InstrumentationEngine_EXTENSION_VERSION" = "~1"
+		"SnapshotDebugger_EXTENSION_VERSION" = "~1"
+		"XDT_MicrosoftApplicationInsights_BaseExtensions" = "~1"
+		"XDT_MicrosoftApplicationInsights_Mode" = "recommended"
+		"WEBSITE_RUN_FROM_PACKAGE" = "1"
+	}
+}
+
+resource "azurerm_app_service_slot" "beta" {
+	name		= "beta"
+	location	= "${azurerm_resource_group.rg.location}"
+	resource_group_name = "${azurerm_resource_group.rg.name}"
+	app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+	app_service_name = "${azurerm_app_service.web.name}"
+	https_only = "true"
+
+	app_settings = {
+		"AzureCosmosDbOptions:ConnectionString" = "AccountEndpoint=${azurerm_cosmosdb_account.cosmos.endpoint};AccountKey=${azurerm_cosmosdb_account.cosmos.primary_master_key};"
+		"AzureCosmosDbOptions:DatabaseId" = "cloudskills"
+		"AzureQueueStorageEventPublisherOptions:ConnectionString" = "${azurerm_storage_account.storage.primary_connection_string}"
+		"AzureQueueStorageEventPublisherOptions:QueuePrefix" = ""
+		"AzureTableStorageOptions:ConnectionString" = "${azurerm_storage_account.storage.primary_connection_string}"
+		"ApplicationInsights:InstrumentationKey" = "${azurerm_application_insights.ai.instrumentation_key}"
+		"AzureADB2COptions:Authority" = "${local.b2c_root_uri}/${var.b2c_policy}/v2.0"
+		"AzureADB2COptions:ClientId" = "${var.b2c_client_id}"
+		"AzureADB2COptions:ClientSecret" = "${var.b2c_client_secret}"
+		"AzureADB2COptions:MetadataAddress" = "${local.b2c_root_uri}/v2.0/.well-known/openid-configuration?p=${var.b2c_policy}"
+		"APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.ai.instrumentation_key}"
+		"APPINSIGHTS_PROFILERFEATURE_VERSION" = "1.0.0"
+		"APPINSIGHTS_SNAPSHOTFEATURE_VERSION" = "1.0.0"
+		"ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
+		"DiagnosticServices_EXTENSION_VERSION" = "~3"
+		"InstrumentationEngine_EXTENSION_VERSION" = "~1"
+		"SnapshotDebugger_EXTENSION_VERSION" = "~1"
+		"XDT_MicrosoftApplicationInsights_BaseExtensions" = "~1"
+		"XDT_MicrosoftApplicationInsights_Mode" = "recommended"
+		"WEBSITE_RUN_FROM_PACKAGE" = "1"
 	}
 }
 
 resource "azurerm_function_app" "func" {
-	name = "cloudskills-${var.instance_name}-func"
+	name = "${local.name_prefix}-func"
 	location = "${azurerm_resource_group.rg.location}"
 	resource_group_name = "${azurerm_resource_group.rg.name}"
 	app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
 	storage_connection_string = "${azurerm_storage_account.storage.primary_connection_string}"
+	version = "~2"
 
 	app_settings = {
 		"AzureCosmosDbOptions:ConnectionString" = "AccountEndpoint=${azurerm_cosmosdb_account.cosmos.endpoint};AccountKey=${azurerm_cosmosdb_account.cosmos.primary_master_key};"
@@ -50,7 +99,7 @@ resource "azurerm_function_app" "func" {
 		"AzureQueueStorageEventPublisherOptions:QueuePrefix" = ""
 		"AzureStorageOptions:ConnectionString" = "${azurerm_storage_account.storage.primary_connection_string}"
 		"ApplicationInsights:InstrumentationKey" = "${azurerm_application_insights.ai.instrumentation_key}"
-		"APPLICATION_INSIGHTS" = "${azurerm_application_insights.ai.instrumentation_key}"
+		"APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.ai.instrumentation_key}"
 		"APPINSIGHTS_PROFILERFEATURE_VERSION" = "1.0.0"
 		"APPINSIGHTS_SNAPSHOTFEATURE_VERSION" = "1.0.0"
 		"ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
@@ -60,6 +109,11 @@ resource "azurerm_function_app" "func" {
 		"XDT_MicrosoftApplicationInsights_BaseExtensions" = "~1"
 		"XDT_MicrosoftApplicationInsights_Mode" = "recommended"
 		"FUNCTIONS_EXTENSION_VERSION" = "~2"
+		"WEBSITE_RUN_FROM_PACKAGE" = "1"
+	}
+
+	site_config {
+		always_on = "true"
 	}
 }
 
@@ -78,7 +132,7 @@ resource "azurerm_storage_table" "table" {
 }
 
 resource "azurerm_cosmosdb_account" "cosmos" {
-	name = "cloudskills-${var.instance_name}-cosmosdb"
+	name = "${local.name_prefix}-cosmosdb"
 	resource_group_name = "${azurerm_resource_group.rg.name}"
 	location = "${azurerm_resource_group.rg.location}"
 	offer_type = "Standard"
@@ -94,7 +148,7 @@ resource "azurerm_cosmosdb_account" "cosmos" {
 }
 
 resource "azurerm_application_insights" "ai" {
-	name = "cloudskills-${var.instance_name}-ai"
+	name = "${local.name_prefix}-ai"
 	resource_group_name = "${azurerm_resource_group.rg.name}"
 	location = "${azurerm_resource_group.rg.location}"
 	application_type = "other"
--- Deployments/terraform/variables.tf
+++ Deployments/terraform/variables.tf
@@ -2,3 +2,13 @@ variable "instance_name" {}
 variable "location" {
 	default = "westus2"
 }
+variable "b2c_tenant" {
+	default = "cloudskillsouthwest"
+}
+variable "b2c_policy" {
+	default = "B2C_1_SignUpSignIn"
+}
+variable "b2c_client_id" {
+	default = "2e941d7a-fe74-4aa6-9881-9f70364a7cc3"
+}
+variable "b2c_client_secret" {}
