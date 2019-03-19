557a3265860d6addf28a0a227f11f2e23a6efa57
Nick Patterson <nkpatterson@live.com>
2019-03-18 17:29:41 -0700

    Updated configCosmos.sh to check if DB already exists

--- Deployments/configCosmos.sh
+++ Deployments/configCosmos.sh
@@ -10,10 +10,16 @@ done
 dbid="cloudskills"
 collections=(leaderboards contestlearners contests learners courses)
 ru="400"
+exists=$(az cosmosdb database exists -n $NAME -g $GROUP -d $dbid)
+
+if [ $exists = "true" ]
+then
+    echo "Database $dbid already exists. Exiting."
+    exit 0
+fi
 
 echo "Creating database $dbid"
 az cosmosdb database create -n $NAME -g $GROUP -d $dbid
-
 
 for i in "${collections[@]}"
 do
